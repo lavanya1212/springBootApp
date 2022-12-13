546b57900aa6acdfd2ba4af7e3317ed7e646fc3d
lavanya1212 <lavi1212@gmail.com>
2022-12-12 23:09:22 -0700

    committing files

---
+++ src/main/java/com/example/springBootApp/controller/AppController.java
@@ -0,0 +1,15 @@
+package com.example.springBootApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AppController {
+
+	@RequestMapping("/welcome")
+	@ResponseBody
+	public String entryPoint() {
+		return "Welcome to the Spring Boot app";
+	}
+}
